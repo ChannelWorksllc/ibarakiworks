577c429ba6661488631bc383882e856133d5af79
ayumihayami <ayuxum1.33@gmail.com>
2021-10-20 02:08:14 +0900

    レスポンシブ調整中

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,26 @@ $(function(){
     });
 });
 
+//ピックアップコンテンツ//
+$(function() {
+    var swiper; 
+    $(window).on('load resize', function(){
+        var w = $(window).width();
+        if (w <= 979) {
+            if (swiper) {
+                return;
+            } else {
+                swiper = new Swiper('.swiper-container', {
+                    slidesPerView: 2.0,
+                    centeredSlides: true,
+                    initialSlide: 1,
+                });
+            }
+        } else {
+            if (swiper) {
+                swiper.destroy();
+                swiper = undefined;
+            } 
+        } 
+    });
+}); 
